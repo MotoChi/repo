a7beb81b5f30b4db5712ab7a568866be1b60c572
ccaper <christian.caperton@gmail.com>
2012-11-26 00:50:04 +0000

    initial commit

---
+++ GitTest/GitTest/src/com/ccaper/GitTest/HelloGit.java
@@ -0,0 +1,13 @@
+package com.ccaper.GitTest;
+
+public class HelloGit {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
