b59b69cf70f5687af7ad45983a967cc64f86dd26
ccaper <christian.caperton@gmail.com>
2012-11-27 00:10:38 +0000

    adding even more output, this time from vm

--- GitTest/GitTest/src/com/ccaper/GitTest/HelloGit.java
+++ GitTest/GitTest/src/com/ccaper/GitTest/HelloGit.java
@@ -4,5 +4,6 @@ public class HelloGit {
   public static void main(String[] args) {
     System.out.println("Git is cool.");
     System.out.println("I mean really cool.");
+    System.out.println("I mean so coo I'm shittin' my pants.");
   }
 }
