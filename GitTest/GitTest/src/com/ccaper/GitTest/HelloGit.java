8f7724f906d68020cee4652efddf158f06a71b8d
ccaper <ccaper@gmail.com>
2012-11-26 18:00:56 -0600

    adding more output from work laptop

--- GitTest/GitTest/src/com/ccaper/GitTest/HelloGit.java
+++ GitTest/GitTest/src/com/ccaper/GitTest/HelloGit.java
@@ -3,5 +3,6 @@ package com.ccaper.GitTest;
 public class HelloGit {
   public static void main(String[] args) {
     System.out.println("Git is cool.");
+    System.out.println("I mean really cool.");
   }
 }
